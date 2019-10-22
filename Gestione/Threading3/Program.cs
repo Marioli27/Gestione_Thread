205cf6a11632824d54ce53a20c298930fcb1e39f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:45:08 +0200

    terzo esempio

---
+++ Gestione/Threading3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someVariable = "Matteo Tumiati";
+
+            var workerThread = new Thread(() =>
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Saluti da: {0}", someVariable);
+
+            });
+            workerThread.Start();
+            someVariable = "Daniele bochicchio";
+
+            Console.ReadLine();
+        }
+    }
+}
