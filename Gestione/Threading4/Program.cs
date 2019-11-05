1dba811decedf2e83babdd3d5f459a1ef47c9f05
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-05 08:40:09 +0100

    Thread numero 4

---
+++ Gestione/Threading4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Thread>();
+
+            // Qui creiamo ed eseguiamo cinque worker thread 
+            for (int index = 0; index < 5; index++)
+           
+            {
+                var myThread = new Thread((currentIndex) =>
+                {
+                    Console.WriteLine("Thread {0} è iniziato", currentIndex);
+                    Thread.Sleep(500);
+                    Console.WriteLine("Thread {0} è terminato", currentIndex);
+                });
+                    myThread.Start(index);
+                    list.Add(myThread);
+            }
+
+            foreach (Thread thread in list)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("Esecuzione di tutti i thread terminata");
+            
+        }
+    }
+}
